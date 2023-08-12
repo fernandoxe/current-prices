16d4f62baf5357072cdea7b096601f364c41eba8
Fernando Parra <fernandoxe@gmail.com>
2023-08-12 02:38:27 +0200

    chore: added prices for second sale

---
+++ src/components/app-group-prices/index.ts
@@ -0,0 +1,221 @@
+import { LitElement, css, html } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+
+@customElement('app-group-prices')
+export class AppGroupPrices extends LitElement {
+  static styles = css`
+    h4 {
+      margin: 0;
+      margin-bottom: 0.875rem;
+      font-size: 0.875rem;
+    }
+    .container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 0.875rem;
+    }
+
+    .row {
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      gap: 0.875rem;
+    }
+    .row > div {
+      width: 33.33%;
+    }
+
+    .row-title {
+      font-size: 0.75rem;
+    }
+    
+    .row-title > div:first-child {
+      font-weight: bold;
+    }
+    .no-vip {
+      font-size: 0.75rem;
+      text-align: center;
+    }
+
+    .column-title {
+      font-weight: bold;
+      font-size: 0.75rem;
+      text-align: center;
+    }
+
+    .colored {
+      color: #6d28d9;
+      /* color: #4a9bd7; */
+    }
+    
+    .loader {
+      width: 100%;
+      height: 20px;
+      background: linear-gradient(90deg, #cccccc 25%, #e9e9e9 50%, #cccccc 75%);
+      background-size: 200% 100%;
+      animation: loading 1.5s infinite;
+    }
+    .loader.usd-today {
+      width: 1.5rem;
+      height: 0.875rem;
+      display: inline-block;
+      vertical-align: middle;
+    }
+    @keyframes loading {
+      0% {
+        background-position: 200% 0;
+      }
+      100% {
+        background-position: -200% 0;
+      }
+    }
+  `;
+
+  @property({type: Object})
+  date = {
+    sale: '',
+    today: '',
+  };
+
+  @property({type: Object})
+  usdValue = {
+    sale: 0,
+    today: 0,
+  };
+
+  @property({type: Object})
+  peso = {
+    price: 0,
+    serviceCharge: 0,
+    vip: 0,
+  };
+
+  @property({type: Object})
+  usd = {
+    price: 0,
+    vip: 0,
+  };
+
+  @property({type: Object})
+  recommended = {
+    price: 0,
+    vip: 0,
+  };
+
+  render() {
+    return html`
+      <h4>
+        Entradas compradas el ${this.date.sale}:
+      </h4>
+      <div class="container">
+        <div class="row">
+          <div class="column-title">
+          </div>
+          <div class="column-title">
+            General
+          </div>
+          <div class="column-title">
+            VIP
+          </div>
+        </div>
+        <div class="row">
+          <div class="row-title">
+            <div>
+              Venta general
+            </div>
+            <div>
+              ${this.date.sale}
+            </div>
+          </div>
+          <div>
+            <app-price
+              price=${`$ ${this.peso.price + this.peso.serviceCharge}`}
+              bottom-info=${`$${this.peso.price} + $${this.peso.serviceCharge} service charge`}
+            ></app-price>
+          </div>
+          <div>
+            ${this.peso.vip > 0 ?
+              html`<app-price
+                price=${`$ ${this.peso.vip}`}
+                bottom-info=${`Los VIP no tienen service charge`}
+              ></app-price>`
+              :
+              html``
+            }
+          </div>
+        </div>
+        <div class="row">
+          <div class="row-title">
+            <div>
+              En dólares
+            </div>
+            <div>
+              (dólar ${this.date.sale} $${this.usdValue.sale})
+            </div>
+          </div>
+          <div>
+            <app-price
+              price=${`us$ ${this.usd.price}`}
+            ></app-price>
+          </div>
+          <div>
+            ${this.peso.vip > 0 ?
+              html`<app-price
+                price=${`us$ ${this.usd.vip}`}
+              ></app-price>`
+              :
+              html`<div class="no-vip">No hubo VIP para este sector</div>`
+            }
+          </div>
+        </div>
+        ${this.usdValue.today > 0 ?
+          html`
+            <div class="row">
+              <div class="row-title">
+                <div>
+                  Precio de reventa recomendado
+                </div>
+                <div>
+                  (dólar hoy $${this.usdValue.today})
+                </div>
+              </div>
+              <div class="colored">
+                <app-price
+                  price=${`$ ${this.recommended.price}`}
+                ></app-price>
+              </div>
+              <div class="colored">
+                ${this.peso.vip > 0 ?
+                  html`<app-price
+                    price=${`$ ${this.recommended.vip}`}
+                  ></app-price>`
+                  :
+                  html``
+                }
+              </div>
+            </div>
+          `
+          :
+          html`
+            <div class="row">
+              <div class="row-title">
+                <div>
+                  Precio de reventa recomendado
+                </div>
+                <div>
+                  (dólar hoy <span class="loader usd-today"></span>)
+                </div>
+              </div>
+              <div class="loader">
+              </div>
+              <div class="loader">
+              </div>
+            </div>
+          `
+        }
+      </div>
+    `;
+  }
+}
--- src/components/app-prices/index.ts
+++ src/components/app-prices/index.ts
@@ -3,62 +3,21 @@ import { customElement, property, state } from 'lit/decorators.js';
 import { VIPPackagesRanges, stadiumRanges, stadiumSectors } from '../../data';
 import '../app-price';
 import { styleMap } from 'lit/directives/style-map.js';
+import '../app-group-prices';
+import { SALE_DAY_1, SALE_DAY_2, USD_VALUE_1, USD_VALUE_2 } from '../../data';
 
 @customElement('app-prices')
 export class AppPrices extends LitElement {
   static styles = css`
-    h3, h4 {
-      margin: 0;
-    }
     h3 {
+      margin: 0;
       font-size: 1rem;
     }
     .container {
       display: flex;
       flex-direction: column;
       align-items: center;
-      gap: 1rem;
-    }
-    .general-container {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      gap: 0.5rem;
-    }
-    .prices {
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
       gap: 1.5rem;
-    }
-    .row {
-      width: 100%;
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      gap: 1rem;
-    }
-    .row > div {
-      width: 33.33%;
-    }
-    .column-title {
-      font-weight: bold;
-      font-size: 0.75rem;
-      text-align: center;
-    }
-    .row-title {
-      font-size: 0.75rem;
-    }
-    .row-title > div:first-child {
-      font-weight: bold;
-    }
-    .no-vip {
-      font-size: 0.75rem;
-      text-align: center;
-    }
-    .first-column {
-      width: 20%;
     }
     .square {
       display: inline-block;
@@ -69,34 +28,10 @@ export class AppPrices extends LitElement {
     .text {
       vertical-align: middle;
     }
-    .colored {
-      color: #6d28d9;
-    }
     .final {
       font-size: 0.75rem;
       font-style: italic;
       margin-top: 1rem;
-    }
-    .loader {
-      width: 100%;
-      height: 20px;
-      background: linear-gradient(90deg, #cccccc 25%, #e9e9e9 50%, #cccccc 75%);
-      background-size: 200% 100%;
-      animation: loading 1.5s infinite;
-    }
-    .loader.usd-today {
-      width: 1rem;
-      height: 0.875rem;
-      display: inline-block;
-      vertical-align: middle;
-    }
-    @keyframes loading {
-      0% {
-        background-position: 200% 0;
-      }
-      100% {
-        background-position: -200% 0;
-      }
     }
   `;
 
@@ -110,13 +45,25 @@ export class AppPrices extends LitElement {
   private vip = '';
 
   @state()
-  private price = {
+  private peso = [{
     price: 0,
     serviceCharge: 0,
-    vipPrice: 0,
-    usd: 0,
-    vipUsd: 0,
-  };
+    vip: 0,
+  }];
+
+  @state()
+  private usd = [{
+    price: 0,
+    vip: 0,
+  }];
+
+  @state()
+  private recommended = [
+    {
+      price: 0,
+      vip: 0,
+    }
+  ];
 
   @state()
   private color = '';
@@ -137,14 +84,62 @@ export class AppPrices extends LitElement {
     this.color = stadiumRanges[priceRange]?.color || '';
     const serviceCharge = stadiumRanges[priceRange]?.['service-charge'] || 0;
     const vipPrice = VIPPackagesRanges[this.vip]?.price || 0;
+
+    const serviceChargeValues = [
+      price * serviceCharge,
+      price * serviceCharge,
+    ];
+
+    const priceValues = [
+      price + serviceChargeValues[0],
+      price + serviceChargeValues[1],
+    ];
+
+    const usdValues = [
+      {
+        price: priceValues[0] / USD_VALUE_1,
+        vip: vipPrice / USD_VALUE_1,
+      },
+      {
+        price: priceValues[1] / USD_VALUE_2,
+        vip: vipPrice / USD_VALUE_2,
+      },
+    ];
+
+    this.peso = [
+      {
+        price,
+        serviceCharge: serviceChargeValues[0],
+        vip: vipPrice,
+      },
+      {
+        price,
+        serviceCharge: serviceChargeValues[1],
+        vip: 0,
+      },
+    ];
     
-    this.price = {
-      price,
-      serviceCharge: price * serviceCharge,
-      vipPrice,
-      usd: Math.round((price + price * serviceCharge) / 485),
-      vipUsd: Math.round(vipPrice / 485),
-    };
+    this.usd = [
+      {
+        price: Math.round(priceValues[0] / USD_VALUE_1),
+        vip: Math.round(vipPrice / USD_VALUE_1),
+      },
+      {
+        price: Math.round(priceValues[1] / USD_VALUE_2),
+        vip: Math.round(vipPrice / USD_VALUE_2),
+      },
+    ];
+
+    this.recommended = [
+      {
+        price: Math.round(usdValues[0].price * this.usdToday),
+        vip: Math.round(usdValues[0].vip * this.usdToday),
+      },
+      {
+        price: Math.round(usdValues[1].price * this.usdToday),
+        vip: Math.round(usdValues[1].vip * this.usdToday),
+      },
+    ];
   }
 
   protected performUpdate(): void | Promise<unknown> {
@@ -153,127 +148,34 @@ export class AppPrices extends LitElement {
   }
 
   render() {
-    if(!this.price.price) return html``;
+    if(!this.peso[0].price) return html``;
     return html`
-    <div class="container">
-      <h3>
-        <span class="square" style=${styleMap({'background-color': this.color})}>
-        </span>
-        <span class="text">
-          ${this.priceid ? stadiumSectors[this.priceid].name : ''}
-        </span>
-      </h3>
-      <div class="prices">
-        <div class="row">
-          <div class="column-title first-column">
-          </div>
-          <div class="column-title">
-            General
-          </div>
-          <div class="column-title">
-            VIP
-          </div>
+      <div class="container">
+        <h3>
+          <span class="square" style=${styleMap({'background-color': this.color})}>
+          </span>
+          <span class="text">
+            ${this.priceid ? stadiumSectors[this.priceid].name : ''}
+          </span>
+        </h3>
+        <app-group-prices
+          .date=${{sale: SALE_DAY_1, today: this.getToday()}}
+          .usdValue=${{sale: USD_VALUE_1, today: this.usdToday}}
+          .peso=${this.peso[0]}
+          .usd=${this.usd[0]}
+          .recommended=${this.recommended[0]}
+        ></app-group-prices>
+        <app-group-prices
+          .date=${{sale: SALE_DAY_2, today: this.getToday()}}
+          .usdValue=${{sale: USD_VALUE_2, today: this.usdToday}}
+          .peso=${this.peso[1]}
+          .usd=${this.usd[1]}
+          .recommended=${this.recommended[1]}
+        ></app-group-prices>
+        <div class="final">
+          Los precios de reventa recomendados están calculados en base al valor del dólar de hoy ${this.getToday()}.
         </div>
-        <div class="row">
-          <div class="row-title first-column">
-            <div>
-              Venta general
-            </div>
-            <div>
-              (6 de junio)
-            </div>
-          </div>
-          <div>
-            <app-price
-              price=${`$ ${this.price.price + this.price.serviceCharge}`}
-              bottom-info=${`$${this.price.price} + $${this.price.serviceCharge} service charge`}
-            ></app-price>
-          </div>
-          <div>
-            ${this.vip ?
-              html`<app-price
-                price=${`$ ${this.price.vipPrice}`}
-                bottom-info=${`Los VIP no tienen service charge`}
-              ></app-price>`
-              :
-              ''
-            }
-          </div>
-        </div>
-        <div class="row">
-          <div class="row-title first-column">
-            <div>
-              En dólares
-            </div>
-            <div>
-              (dólar 6 de junio $485)
-            </div>
-          </div>
-          <div>
-            <app-price
-              price=${`us$ ${this.price.usd}`}
-            ></app-price>
-          </div>
-          <div>
-            ${this.vip ?
-              html`<app-price
-                price=${`us$ ${this.price.vipUsd}`}
-              ></app-price>`
-              :
-              html`<div class="no-vip">Este sector no tiene VIP</div>`
-            }
-          </div>
-        </div>
-        ${this.usdToday > 0 ?
-          html`
-            <div class="row">
-              <div class="row-title">
-                <div>
-                  Precio de reventa recomendado
-                </div>
-                <div>
-                  (dólar hoy $${this.usdToday})
-                </div>
-              </div>
-              <div class="colored">
-                <app-price
-                  price=${`$ ${+(this.usdToday * this.price.usd).toFixed(2)}`}
-                ></app-price>
-              </div>
-              <div class="colored">
-                ${this.vip ?
-                  html`<app-price
-                    price=${`$ ${+(this.usdToday * this.price.vipUsd).toFixed(2)}`}
-                  ></app-price>`
-                  :
-                  ''
-                }
-              </div>
-            </div>
-            <div class="final">
-              Los precios recomendados están calculados en base al valor del dólar de hoy ${this.getToday()}.
-            </div>
-          `
-          :
-          html`
-            <div class="row">
-              <div class="row-title">
-                <div>
-                  Precio de reventa recomendado
-                </div>
-                <div>
-                  (dólar hoy <span class="loader usd-today"></span>)
-                </div>
-              </div>
-              <div class="loader">
-              </div>
-              <div class="loader">
-              </div>
-            </div>
-          `
-        }
       </div>
-    </div>
     `;
   }
 }
